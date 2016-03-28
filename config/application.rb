ab5b4d993eae9af4c49f6a8562d89e63c28498b9
skydiverhz <skydiver@pipeline.com>
2016-03-28 11:34:14 -0400

    HW Submission by Harold Zuckerman

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-<<<<<<< HEAD
-module Lesson09HomeworkAllCrudActionsAndHandlingErrors
-=======
 module Lesson07HomeworkCreatingArticles
->>>>>>> origin/master
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
